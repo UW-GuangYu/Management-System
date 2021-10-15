b225af3240896e71697bbe536b565a29217bed15
Guangyu Wu <>
2021-10-15 17:50:02 +0800

    Created the interaction between front end and back end. Implemented some functions. (Query ,  Add) 增，查

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -3,7 +3,7 @@
 
     <!--      功能区域-->
     <div style="margin: 10px 0">
-      <el-button type="primary">新增</el-button>
+      <el-button type="primary" @click="add">新增</el-button>
       <el-button type="primary">导入</el-button>
       <el-button type="primary">导出</el-button>
     </div>
@@ -11,17 +11,20 @@
 
     <!--    搜索区域-->
     <div style="margin: 10px 0">
-      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%"></el-input>
-      <el-button type="primary" style="margin-left: 5px">查询</el-button>
+      <el-input v-model="search" placeholder="请输入关键字" style="width: 20%" clearable></el-input>
+      <el-button type="primary" style="margin-left: 5px" @click="load">查询</el-button>
     </div>
 
 
     <el-table :data="tableData" border stripe style="width: 100%">
-      <el-table-column prop="date" label="日期" sortable/>
-      <el-table-column prop="name" label="姓名"/>
+      <el-table-column prop="id" label="ID" sortable/>
+      <el-table-column prop="username" label="用户名"/>
+      <el-table-column prop="nickName" label="昵称"/>
+      <el-table-column prop="age" label="年龄"/>
+      <el-table-column prop="sex" label="性别"/>
       <el-table-column prop="address" label="地址"/>
 
-      <el-table-column fixed="right" label="操作">
+      <el-table-column fixed="right" label="">
         <template #default>
           <el-button size="mini" @click="handleEdit">编辑</el-button>
 
@@ -38,7 +41,7 @@
       <el-pagination
           v-model:currentPage="currentPage"
           :page-sizes="[5, 10, 20]"
-          :page-size="10"
+          :page-size="pageSize"
           layout="total, sizes, prev, pager, next, jumper"
           :total="total"
           @size-change="handleSizeChange"
@@ -46,6 +49,35 @@
       >
       </el-pagination>
     </div>
+
+    <el-dialog v-model="dialogVisible" title="提示" width="30%">
+      <el-form :model="form" label-width="120px">
+        <el-form-item label="用户名">
+          <el-input v-model="form.username" style="width: 80%"></el-input>
+        </el-form-item>
+        <el-form-item label="昵称">
+          <el-input v-model="form.nickName" style="width: 80%"></el-input>
+        </el-form-item>
+        <el-form-item label="年龄">
+          <el-input v-model="form.age" style="width: 80%"></el-input>
+        </el-form-item>
+        <el-form-item label="性别">
+          <el-radio v-model="form.sex" label="男">男</el-radio>
+          <el-radio v-model="form.sex" label="女">女</el-radio>
+          <el-radio v-model="form.sex" label="未知">未知</el-radio>
+        </el-form-item>
+        <el-form-item label="地址">
+          <el-input type="textarea" v-model="form.address" style="width: 80%"></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="save">Confirm</el-button>
+      </span>
+      </template>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -57,48 +89,57 @@
 
 <script>
 
+import request from "@/utils/request";
+
 export default {
   name: "Home",
   components: {},
 
   data() {
     return {
+      form: {},
+      dialogVisible: false,
       search: '',
       currentPage: 1,
-      total: 10,
-      tableData: [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        }
-      ]
+      pageSize: 10,
+      total: 0,
+      tableData: []
     }
+  },
+  created() {
+    this.load()
   },
   methods: {
     handleEdit() {
 
     },
-    handleSizeChange(){
+    handleSizeChange() {
 
     },
-    handleCurrentChange(){
+    handleCurrentChange() {
 
+    },
+    add() {
+      this.dialogVisible = true;
+      this.form = {};
+    },
+    save(){
+      request.post("/api/user", this.form).then(res => {
+        console.log(res);
+      })
+    },
+    load(){
+      request.get("api/user", {
+        params: {
+          pageNum: this.currentPage,
+          pageSize: this.pageSize,
+          search: this.search
+        }
+      }).then(res =>{
+        console.log(res);
+        this.tableData = res.data.records;
+        this.total = res.data.total;
+      })
     }
   }
 }
